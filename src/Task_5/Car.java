9f549dc608857e04b65a3c612fc3a6068380a3f5
Valery_Bezrukov <valgmal@gmail.com>
2020-07-29 01:30:25 +0300

    Task5

--- src/Task_5/Car.java
+++ src/Task_5/Car.java
@@ -5,7 +5,6 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
-
     public String getName() {
         return name;
     }
@@ -24,7 +23,6 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-//            MoveController.startBarrier.countDown();
             MoveController.startBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
