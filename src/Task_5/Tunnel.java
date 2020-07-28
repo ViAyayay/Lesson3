a615c0c945f1bce2bb88d604cf6d25df37e31414
Valery_Bezrukov <valgmal@gmail.com>
2020-07-28 23:54:34 +0300

    Task 5. base

---
+++ src/Task_5/Tunnel.java
@@ -0,0 +1,24 @@
+package Task_5;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car car) {
+        try {
+            try {
+                System.out.println(car.getName() + " готовится к этапу(ждет): " + description);
+                System.out.println(car.getName() + " начал этап: " + description);
+                Thread.sleep(length / car.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(car.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
